2ce8ecc07208e6a4675d96c8d892f0958d030060
Keyvi Caisa Risman <127940133+KyvCR@users.noreply.github.com>
2023-07-08 22:14:12 +0700

    Update DAG_ELT.py

--- dags/DAG_ELT.py
+++ dags/DAG_ELT.py
@@ -24,13 +24,15 @@ default_args = {
     'start_date': datetime(2023, 7, 1),
     'retries': 1,
     'retry_delay': timedelta(seconds=10),
-    'catchup': False
+    'catchup': False,
+    'email': ['keyvicaisarisman@gmail.com'],
+    'email_on_failure': True, #jika failure akan mengirimkan email alert ke parameter email diatas
 }
 
 dag = DAG(
     dag_id='data_covid',
     default_args=default_args,
-    schedule_interval='0 0 * * *'
+    schedule_interval='0 0 * * *' #akan berjalan setiap hari pada pukul 00:00 atau tengah malam
 )
 
 
@@ -42,7 +44,6 @@ with dag:
         python_callable=buat_list,
     )
 
-   
 
     with TaskGroup(group_id='Load') as Load:
         task2 = PythonOperator(
@@ -54,37 +55,19 @@ with dag:
             task_id = 'insert_data',
             python_callable=insert_raw_data,
         )
-        # task3 = PythonOperator(
-        #     task_id = 'Createtable_Country',
-        #     python_callable=createtable_Country,
-        # )
-        # task4 = PythonOperator(
-        #     task_id = 'createtable_ActiveCasesData',
-        #     python_callable=createtable_ActiveCasesData,
-        # )
-        # task5 = PythonOperator(
-        #     task_id = 'createtable_NewCaseData',
-        #     python_callable=createtable_NewCaseData,
-        # )
-        # task6 = PythonOperator(
-        #     task_id = 'createtable_DeathsData',
-        #     python_callable=createtable_DeathsData,
-        # )
-
         task2 >> task3
         
-        #[task3, task4, task5, task6]
-
     with TaskGroup(group_id='Trasnform') as Transform:
+        #varuable didapat dari valriable yang ada dia riflow
+        #connection mysqllocal mengambil dari connection yang ada di airflow
 
-        #query_create_table_country = Variable.get('query_create_table_country')
+
         create_table_country = MySqlOperator(
             task_id = 'create_table_country',
-            mysql_conn_id='mysqllocal',
-            sql = Variable.get('query_create_table_country'),
+            mysql_conn_id='mysqllocal', 
+            sql = Variable.get('query_create_table_country'), 
         )
 
-        #query_insert_table_country = Variable.get('query_insert_table_country')
         insert_table_country = MySqlOperator(
             task_id = 'insert_table_country',
             mysql_conn_id='mysqllocal',
@@ -121,53 +104,5 @@ with dag:
         create_table_country >> insert_table_country
         create_table_deaths >> insert_table_deaths
        
-
-    # with TaskGroup(group_id='Load') as Load:
-    #     # task7 = PythonOperator(
-    #     #     task_id = 'insert_data',
-    #     #     python_callable=insert_data(createtable_ActiveCasesData(),'active_case_test'),
-    #     # )    
-
-    #     task8 = PythonOperator(
-    #         task_id = 'insert_data_country',
-    #         python_callable=insert_data_country(),
-    #     )    
-
-        # task9 = PythonOperator(
-        #     task_id = 'insert_data_active_case',
-        #     python_callable=insert_data_active_case(),
-        # )   
-
-        # task10 = PythonOperator(
-        #     task_id = 'insert_data_NewCaseData',
-        #     python_callable=insert_data_NewCaseData(),
-        # )
-
-        # task11 = PythonOperator(
-        #     task_id = 'insert_data_DeathsData',
-        #     python_callable=insert_data_DeathsData(),
-        # )
-
-        
-    #with TaskGroup(group_id="group1") as tg1:
-
-    # task3 = PythonOperator(
-    #         task_id = 'clenasingdata',
-    #         python_callable=cleansing_data,
-    # )
-
-    # task4 = PythonOperator(
-    #         task_id = 'test',
-    #         python_callable=createTable_Country,
-    # )
-
-    # task4 = PythonOperator(
-    #         task_id = 'test',
-    #         python_callable=insert_data,
-    #         op_arg=['createtable_Country()', 'Country']
-    # )
-
-
-   #task3  >> task4
 
 task1 >> Load >> Transform
